35663794fa30b08f075a94b910a25253e459783d
AlcorBBX <lev.matisov55@gmail.com>
2023-06-16 07:24:38 +0500

    feat(config.lua): disable hightlight current line

--- config.lua
+++ config.lua
@@ -2,7 +2,9 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.relativenumber = true
 
+-- add second numbers column
 vim.opt.statuscolumn = "%l %r"
+vim.opt.cursorline = false
 
 -- general
 lvim.log.level = "info"
